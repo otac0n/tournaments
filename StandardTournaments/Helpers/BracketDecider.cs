2ccfd12220850ed542c9057a84f68dee1b92f6e1
John Gietzen <otac0n@gietzen.us>
2009-10-18 16:18:04 +0000

    Started Makinging the tests more readable. Started the structure for the "BracketDecider" class that will understand n-elimination bracket simplification.

---
+++ StandardTournaments/Helpers/BracketDecider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tournaments.Standard.Helpers
+{
+    public class BracketDecider : EliminationDecider
+    {
+        private List<EliminationNode> bracketRootNodes = new List<EliminationNode>();
+
+        public BracketDecider(IEnumerable<EliminationNode> bracketRootNodes)
+        {
+           this.bracketRootNodes.AddRange(bracketRootNodes);
+        }
+
+        public BracketDecider(params EliminationNode[] bracketRootNodes)
+        {
+            this.bracketRootNodes.AddRange(bracketRootNodes);
+        }
+
+        public override bool IsDecided
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override TournamentTeam GetWinner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TournamentTeam GetLoser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ApplyPairing(TournamentPairing pairing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TournamentPairing> FindUndecided()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<EliminationNode> FindNodes(Func<EliminationNode, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<EliminationDecider> FindDeciders(Func<EliminationDecider, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override NodeMeasurement MeasureWinner(Tournaments.Graphics.IGraphics g, TournamentNameTable names, float textHeight, Score score)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override NodeMeasurement MeasureLoser(Tournaments.Graphics.IGraphics g, TournamentNameTable names, float textHeight, Score score)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderWinner(Tournaments.Graphics.IGraphics g, TournamentNameTable names, float x, float y, float textHeight, Score score)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderLoser(Tournaments.Graphics.IGraphics g, TournamentNameTable names, float x, float y, float textHeight, Score score)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TournamentTests/Helpers/ScoreTests.cs
+++ TournamentTests/Helpers/ScoreTests.cs
@@ -7,9 +7,85 @@ using Tournaments;
 
 namespace TournamentTests
 {
-    /// <summary>
-    /// Summary description for ScoreTests
-    /// </summary>
+    [TestClass]
+    public class LowestPointsScoreTests
+    {
+        public LowestPointsScoreTests()
+        {
+        }
+
+        /// <summary>
+        /// Tests that the point value of a LowestPointsScore is exactly the same as the value passed into the constructor.
+        /// </summary>
+        [TestMethod]
+        public void Points_PassesValueThrough()
+        {
+            var scoreA = new LowestPointsScore(1.337);
+
+            Assert.AreEqual(1.337, scoreA.Points);
+        }
+
+        /// <summary>
+        /// Tests the LowestPointsScore class' comparison functions.
+        /// </summary>
+        /// <remarks>
+        /// This method needs to be refactored to be more clear.
+        /// </remarks>
+        [TestMethod]
+        public void LowestScoreWins()
+        {
+            var scoreNull = (LowestPointsScore)null;
+
+            Assert.IsNull(scoreNull + scoreNull);
+
+            var scores = new LowestPointsScore[10];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = new LowestPointsScore((double)i);
+            }
+
+            for (int a = 0; a < scores.Length; a++)
+            {
+                var scoreA = scores[a];
+                var valueA = scoreA.Points;
+
+                Assert.IsTrue(scoreA.CompareTo(scoreNull) > 0);
+                Assert.IsTrue(scoreA > scoreNull);
+                Assert.IsTrue(scoreA >= scoreNull);
+                Assert.IsFalse(scoreA == scoreNull);
+                Assert.IsFalse(scoreA.Equals((object)scoreNull));
+                Assert.IsTrue(scoreA != scoreNull);
+                Assert.IsFalse(scoreA < scoreNull);
+                Assert.IsFalse(scoreA <= scoreNull);
+
+                Assert.IsFalse(scoreNull > scoreA);
+                Assert.IsFalse(scoreNull >= scoreA);
+                Assert.IsFalse(scoreNull == scoreA);
+                Assert.IsTrue(scoreNull != scoreA);
+                Assert.IsTrue(scoreNull < scoreA);
+                Assert.IsTrue(scoreNull <= scoreA);
+
+                Assert.IsTrue(scoreA == scoreA + scoreNull);
+                Assert.IsTrue(scoreA == scoreNull + scoreA);
+
+                for (int b = 0; b < scores.Length; b++)
+                {
+                    var scoreB = scores[b];
+                    var valueB = scoreB.Points;
+
+                    Assert.AreEqual(valueA.CompareTo(valueB), -scoreA.CompareTo(scoreB));
+                    Assert.AreEqual(valueA < valueB, scoreA > scoreB);
+                    Assert.AreEqual(valueA <= valueB, scoreA >= scoreB);
+                    Assert.AreEqual(valueA == valueB, scoreA == scoreB);
+                    Assert.AreEqual(valueA != valueB, scoreA != scoreB);
+                    Assert.AreEqual(valueA.Equals(valueB), scoreA.Equals((object)scoreB));
+                    Assert.AreEqual(valueA >= valueB, scoreA <= scoreB);
+                    Assert.AreEqual(valueA > valueB, scoreA < scoreB);
+                }
+            }
+        }
+    }
+
     [TestClass]
     public class ScoreTests
     {
@@ -59,69 +135,13 @@ namespace TournamentTests
         [TestMethod]
         public void ScoresPassValueThrough()
         {
-            var scoreA = new LowestPointsScore(1.337);
             var scoreB = new HighestPointsScore(1.337);
             var scoreC = new LowestTimeScore(1337);
             var scoreD = new HighestTimeScore(1337);
 
-            Assert.AreEqual(1.337, scoreA.Points);
             Assert.AreEqual(1.337, scoreB.Points);
             Assert.AreEqual(1337, scoreC.Milliseconds);
             Assert.AreEqual(1337, scoreD.Milliseconds);
-        }
-
-        [TestMethod]
-        public void LowestScoreWins()
-        {
-            var scoreNull = (LowestPointsScore)null;
-
-            Assert.IsNull(scoreNull + scoreNull);
-
-            var scores = new LowestPointsScore[10];
-            for (int i = 0; i < scores.Length; i++)
-            {
-                scores[i] = new LowestPointsScore((double)i);
-            }
-
-            for (int a = 0; a < scores.Length; a++)
-            {
-                var scoreA = scores[a];
-                var valueA = scoreA.Points;
-
-                Assert.IsTrue(scoreA.CompareTo(scoreNull) > 0);
-                Assert.IsTrue(scoreA > scoreNull);
-                Assert.IsTrue(scoreA >= scoreNull);
-                Assert.IsFalse(scoreA == scoreNull);
-                Assert.IsFalse(scoreA.Equals((object)scoreNull));
-                Assert.IsTrue(scoreA != scoreNull);
-                Assert.IsFalse(scoreA < scoreNull);
-                Assert.IsFalse(scoreA <= scoreNull);
-                
-                Assert.IsFalse(scoreNull > scoreA);
-                Assert.IsFalse(scoreNull >= scoreA);
-                Assert.IsFalse(scoreNull == scoreA);
-                Assert.IsTrue(scoreNull != scoreA);
-                Assert.IsTrue(scoreNull < scoreA);
-                Assert.IsTrue(scoreNull <= scoreA);
-
-                Assert.IsTrue(scoreA == scoreA + scoreNull);
-                Assert.IsTrue(scoreA == scoreNull + scoreA);
-
-                for (int b = 0; b < scores.Length; b++)
-                {
-                    var scoreB = scores[b];
-                    var valueB = scoreB.Points;
-
-                    Assert.AreEqual(valueA.CompareTo(valueB), -scoreA.CompareTo(scoreB));
-                    Assert.AreEqual(valueA < valueB, scoreA > scoreB);
-                    Assert.AreEqual(valueA <= valueB, scoreA >= scoreB);
-                    Assert.AreEqual(valueA == valueB, scoreA == scoreB);
-                    Assert.AreEqual(valueA != valueB, scoreA != scoreB);
-                    Assert.AreEqual(valueA.Equals(valueB), scoreA.Equals((object)scoreB));
-                    Assert.AreEqual(valueA >= valueB, scoreA <= scoreB);
-                    Assert.AreEqual(valueA > valueB, scoreA < scoreB);
-                }
-            }
         }
 
         [TestMethod]
